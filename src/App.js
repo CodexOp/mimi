c3157d8c3e76a00c4f115ae6b56d29008afe2a19
tusharnagarop <you@example.comtusharnagarop@gmail.com>
2022-10-13 04:34:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import m from "./m.png";
+import axios from "axios"
+import {AiOutlineLike} from "react-icons/ai"
+import {AiOutlineTwitter} from "react-icons/ai"
+import {FaTelegramPlane} from "react-icons/fa"
+import {AiTwotoneLike} from "react-icons/ai"
+import {React, useState, useEffect} from 'react';
 
 function App() {
+  
+  const [allLiked, setallLiked] = useState();
+  const [liked, setLiked] = useState(true);
+
+
+    const count = async() => {
+      try{
+        const res = await axios.get("https://couternew.herokuapp.com/");
+        console.log(res)
+        getcount();
+        }
+        catch(err){
+          console.log(err)
+        }
+        }
+
+
+    const getcount = async() => {
+      try{
+        const res = await axios.get("https://couternew.herokuapp.com/count");
+        console.log(res)
+        setallLiked(res.data);
+        }
+        catch(err){
+          console.log(err)
+        }
+        }
+
+
+        useEffect(()=>{
+          getcount()
+
+        },[])
+
+        useEffect(()=>{
+          setLiked(localStorage.getItem('liked'));
+        },[count])
+
+
+    
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+          <div className='nav'>
+    <p>MIMI</p>
+    <div className='social'>
+      <FaTelegramPlane className='social_icon'/>
+      <AiOutlineTwitter className='social_icon'/>
     </div>
+      </div>
+      
+    <div className="App">
+
+      <div className="feature">       
+      <h2> <li>No Utility</li></h2>
+      <h2> <li>No Usecase</li></h2>
+      </div>
+      <h2>Just</h2>
+     <img src={m} alt='m' />
+     <p className="description">MIMO token is created as a meme showing a picture of what is important to crypto investors more often.</p>
+     {liked ? <AiTwotoneLike className='icon'  onClick={() => setLiked(false)}/> :  <AiOutlineLike className='icon' onClick={() => {setLiked(false); count(); localStorage.setItem('liked', false)}}/>}
+     <h2>{allLiked} Liked</h2>
+    </div>
+    </>
   );
 }
 
