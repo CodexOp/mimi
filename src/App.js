e7ccd01b1bed8784c231efe0538e2f121300e98f
tusharnagarop <you@example.comtusharnagarop@gmail.com>
2022-10-13 18:45:58 +0530

    last commits

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
   return (
     <>
           <div className='nav'>
-    <p>MIMI</p>
+    <p>MIMO</p>
     <div className='social'>
       <a href="https://t.me/mimotoken" target="_blank" rel="norefferer"><FaTelegramPlane className='social_icon'/></a>
       <a href="https://twitter.com/mimotoken" target="_blank" rel="norefferer"> <AiOutlineTwitter className='social_icon'/></a>
@@ -62,16 +62,23 @@ function App() {
       
     <div className="App">
 
-      <div className="feature">       
-      <h2> <li>No Utility</li></h2>
-      <h2> <li>No Usecase</li></h2>
-      </div>
-      <h2>Just</h2>
-     <img src={m} alt='m' />
-     <p className="description">MIMO token is created as a meme showing a picture of what is important to crypto investors more often.</p>
+    <ol>
+    <li><h2> 1.  No Utility</h2></li>
+    <li> <h2> 2.  No Usecase</h2></li>
+     <li> <h2>3. Just M</h2></li>
+     </ol>
+     <p className="description">MIMO token is created as a meme showing a picture of what is im portant to crypto investors more often.</p>
      {liked ? <AiTwotoneLike className='icon'  onClick={() => setLiked(false)}/> :  <AiOutlineLike className='icon' onClick={() => {setLiked(false); count(); localStorage.setItem('liked', false)}}/>}
      <h2>{allLiked} Liked</h2>
+
+   
     </div>
+    <div className='footer_outer'>
+    <div className="footer">
+      <p>MIMO © 2022</p>
+      <a href="mailto:info@mimotoken.com"><p>Info@mimotoken.com</p></a>
+     </div>
+     </div>
     </>
   );
 }
